5c0d5eab40fcd88b04c69fe9bb925d9acf9f8388
Brandon <N333D637@wichita.edu>
2025-10-06 15:34:44 -0500

    Cleaned up the Next.js page to be entirely blank.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: join(__dirname),
+  },
 };
 
 export default nextConfig;
